232b7dacf31c8fd71f39849584135b2acdb6788e
nivedha1 <r.nivedha1@gmail.com>
2017-03-21 21:09:22 +0530

    changing static folder

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 
-
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
